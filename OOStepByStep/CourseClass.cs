fef6973ef27a9b65c79632ad662dfabba246a183
Xu DU <XDu3@slb.com>
2022-10-27 20:00:23 +0800

    feat: can add teacher

---
+++ OOStepByStep/CourseClass.cs
@@ -0,0 +1,18 @@
+﻿namespace OOStepByStep;
+
+public class CourseClass
+{
+    public string ClassName { get; }
+    private Teacher classTeacher;
+
+    public CourseClass(string className)
+    {
+        ClassName = className;
+    }
+
+    public bool AddTeacher(Teacher teacher)
+    {
+        classTeacher = teacher;
+        return true;
+    }
+}
---
+++ OOStepByStepTest/CourseClassTest.cs
@@ -0,0 +1,22 @@
+﻿using OOStepByStep;
+using Xunit;
+
+namespace OOStepByStepTest
+{
+    public class CourseClassTest
+    {
+        [Fact]
+        public void Should_return_true_when_add_teacher_given_teacher_to_class()
+        {
+            //given
+            var teacher = new Teacher("Amy", 30);
+            var courseClass = new CourseClass("Class 2");
+            //when
+
+            var result = courseClass.AddTeacher(teacher);
+
+            //then
+            Assert.True(result);
+        }
+    }
+}

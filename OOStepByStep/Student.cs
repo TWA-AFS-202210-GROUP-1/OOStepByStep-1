c55e4fc16fbee480208c2acb134368b7d2d62760
Qiu Wan <QWan@slb.com>
2022-10-27 19:15:52 +0800

    feat: implement student introduction test and add person introduction test

--- OOStepByStep/Student.cs
+++ OOStepByStep/Student.cs
@@ -4,8 +4,14 @@ namespace OOStepByStep
 {
     public class Student : Person
     {
+        private string profession = "student";
         public Student(string name, int age) : base(name, age)
         {
+        }
+
+        public override string Introduce()
+        {
+            return $"My name is {Name}. I am {Age} years old. I am a {profession}.";
         }
     }
 }
--- OOStepByStepTest/IntroduceTest.cs
+++ OOStepByStepTest/IntroduceTest.cs
@@ -6,11 +6,25 @@ namespace OOStepByStepTest
     public class IntroduceTest
     {
         [Fact]
+        public void Should_return_person_introduction_When_call_introduce_Given_person_name_age()
+        {
+            //given
+            Person person = new Person(name: "Tom", age: 18);
+            string expectationIntroduction = "My name is Tom. I am 18 years old.";
+
+            //when
+            string personIntroduction = person.Introduce();
+
+            //then
+            Assert.Equal(expectationIntroduction, personIntroduction);
+        }
+
+        [Fact]
         public void Should_return_student_introduction_When_call_introduce_Given_student_name_age()
         {
             //given
-            Student student = new Student(name: "Tom", age: 21);
-            string expectationIntroduction = "My name is Tom. I am 21 years old.";
+            Student student = new Student(name: "Tom", age: 18);
+            string expectationIntroduction = "My name is Tom. I am 18 years old. I am a student.";
 
             //when
             string studentIntroduction = student.Introduce();
@@ -27,10 +41,10 @@ namespace OOStepByStepTest
             string expectationIntroduction = "My name is Amy. I am 30 years old. I am a teacher.";
 
             //when
-            string studentIntroduction = teacher.Introduce();
+            string teacherIntroduction = teacher.Introduce();
 
             //then
-            Assert.Equal(expectationIntroduction, studentIntroduction);
+            Assert.Equal(expectationIntroduction, teacherIntroduction);
         }
     }
 }

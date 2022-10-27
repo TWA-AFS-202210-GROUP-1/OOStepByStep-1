3cc56843af489ae32236cc3270e63f628404d7a8
xma12 <xma12@slb.com>
2022-10-27 20:22:00 +0800

    feat: add teacher class number property and test introduce info

--- OOStepByStep/Teacher.cs
+++ OOStepByStep/Teacher.cs
@@ -2,13 +2,21 @@
 {
     public class Teacher : Person
     {
+        private int classNum;
         public Teacher(string name, int age) : base(name, age)
         {
         }
 
+        public Teacher(string name, int age, int classNum) : base(name, age)
+        {
+            this.classNum = classNum;
+        }
+
         public override string Introduce()
         {
-            return $"My name is {Name}. I am {Age} years old. I am a teacher.";
+            return classNum == 0
+                ? $"My name is {Name}. I am {Age} years old. I am a teacher."
+                : $"My name is {Name}. I am {Age} years old. I am a teacher. I am a student of class {classNum}";
         }
     }
 }
--- OOStepByStepTest/TeacherTest.cs
+++ OOStepByStepTest/TeacherTest.cs
@@ -15,5 +15,16 @@ namespace OOStepByStepTest
             //then
             Assert.Equal("My name is Amy. I am 30 years old. I am a teacher.", info);
         }
+
+        [Fact]
+        public void Should_return_teacher_information_when_introduce_give_name_and_age_class()
+        {
+            //give
+            Teacher teacher = new Teacher("Amy", 30, 2);
+            //when
+            string info = teacher.Introduce();
+            //then
+            Assert.Equal("My name is Amy. I am 30 years old. I am a teacher. I am a student of class 2", info);
+        }
     }
 }

024b547ba2aeea2d8db49c854ed60d533edaaec1
Xu DU <XDu3@slb.com>
2022-10-27 20:19:12 +0800

    feat: can add student

--- OOStepByStep/CourseClass.cs
+++ OOStepByStep/CourseClass.cs
@@ -1,19 +1,31 @@
-﻿namespace OOStepByStep;
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace OOStepByStep;
 
 public class CourseClass
 {
     public string ClassName { get; }
     private Teacher classTeacher;
+    private IList<Student> classStudents;
 
     public CourseClass(string className)
     {
         ClassName = className;
+        classStudents = new List<Student>();
     }
 
     public bool AddTeacher(Teacher teacher)
     {
         classTeacher = teacher;
         teacher.CourseClass = this;
+        return true;
+    }
+
+    public bool AddStudent(Student student)
+    {
+        classStudents.Add(student);
+        student.CourseClass = this;
         return true;
     }
 }
--- OOStepByStepTest/CourseClassTest.cs
+++ OOStepByStepTest/CourseClassTest.cs
@@ -18,5 +18,19 @@ namespace OOStepByStepTest
             //then
             Assert.True(result);
         }
+
+        [Fact]
+        public void Should_return_true_when_add_student_given_teacher_to_class()
+        {
+            //given
+            var student = new Student("Tom", 18);
+            var courseClass = new CourseClass("Class 2");
+            //when
+
+            var result = courseClass.AddStudent(student);
+
+            //then
+            Assert.True(result);
+        }
     }
 }

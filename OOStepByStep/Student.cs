b0ed1d33f6e71354eb7f94b89e19c993cdcc18b8
Frank <zongpengchen@gmail.com>
2022-10-27 17:51:46 +0800

    add test AC2 student

---
+++ OOStepByStep/Student.cs
@@ -0,0 +1,21 @@
+﻿namespace OOStepByStep
+{
+    public class Student : Person
+    {
+        private string title;
+
+        public Student()
+        {
+        }
+
+        public Student(string name, int age) : base(name, age)
+        {
+            this.title = "Student";
+        }
+
+        public override string Print()
+        {
+            return null;
+        }
+    }
+}

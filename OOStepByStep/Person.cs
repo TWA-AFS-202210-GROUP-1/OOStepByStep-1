aebfa2e682b7502a964babee01f09d1c6c5c1bc5
Frank <zongpengchen@gmail.com>
2022-10-27 17:43:31 +0800

    add test AC1

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace OOStepByStep
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Person()
+        {
+        }
+
+        public virtual string Print()
+        {
+            return null;
+        }
+    }
+}
--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -6,10 +6,11 @@ namespace OOStepByStepTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Should_get_name_and_age_when_is_a_person_if_given_name_and_age()
         {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+            var person = new Person();
+            var res = person.Print();
+            Assert.Equal("My name is Tom.I am 21 years old.", res);
         }
     }
 }
